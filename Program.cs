b46009076d9b6dd291d54ed7523bc9ca846ed8b6
undrinkable <ha.al.bu@gmail.com>
2017-05-26 18:24:28 -0700

    extract method

--- Program.cs
+++ Program.cs
@@ -7,18 +7,30 @@ namespace TicTacToeApplication
         static void Main(string[] args)
         {
             int[][] ticTacToeBoard = new int[3][];
-            for(int index = 0; index < ticTacToeBoard.Length; index++) {
+            for (int index = 0; index < ticTacToeBoard.Length; index++)
+            {
                 ticTacToeBoard[index] = new int[3];
             }
 
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+            Console.WriteLine();
+
+            PrintBoard(ticTacToeBoard);
+
+        }
+
+        private static void PrintBoard(int[][] board)
+        {
             Console.WriteLine(" ------------- ");
-            for (int x = 0; x < ticTacToeBoard.Length; x++) {
-                for (int y = 0; y < ticTacToeBoard[0].Length; y++) {
-                    Console.Write(" | " + ticTacToeBoard[x][y]);
+            for (int x = 0; x < board.Length; x++)
+            {
+                for (int y = 0; y < board[0].Length; y++)
+                {
+                    Console.Write(" | " + board[x][y]);
                 }
                 Console.Write(" | ");
             }
-            Console.WriteLine(" ------------- ");           
+            Console.WriteLine(" ------------- ");
         }
     }
 }

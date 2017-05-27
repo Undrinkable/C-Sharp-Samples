cc6609ecbac90b8189e6c45a2ddc6d093a22c1e9
undrinkable <ha.al.bu@gmail.com>
2017-05-26 18:36:53 -0700

    add user prompt and update board based on input

--- Program.cs
+++ Program.cs
@@ -17,6 +17,33 @@ namespace TicTacToeApplication
 
             PrintBoard(ticTacToeBoard);
 
+            bool gameOver = false;
+            while (!gameOver)
+            {
+                // get user input
+                Console.WriteLine("What's your move?");
+                Console.WriteLine("Give your answer as two numbers separated by a comma.");
+                Console.WriteLine("Enter END to stop.");
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    gameOver = true;
+                }
+                else
+                {
+                    // convert user input into tic tac toe board coordinates
+                    string[] coordinates = input.Split(',');
+                    int x = Convert.ToInt32(coordinates[0]);
+                    int y = Convert.ToInt32(coordinates[1]);
+
+                    // set the chosen square to 1
+                    ticTacToeBoard[x][y] = 1;
+
+                    // print out the new state of the board
+                    PrintBoard(ticTacToeBoard);
+                }
+            }
         }
 
         private static void PrintBoard(int[][] board)
@@ -28,7 +55,7 @@ namespace TicTacToeApplication
                 {
                     Console.Write(" | " + board[x][y]);
                 }
-                Console.Write(" | ");
+                Console.WriteLine(" | ");
             }
             Console.WriteLine(" ------------- ");
         }
